5984ef242f9043a4d4574f02bf4a77610994e32c
mateedo <mateoshkodra9@gmail.com>
2024-05-26 19:52:48 -0400

    added image to home

--- app/HomePage.js
+++ app/HomePage.js
@@ -1,6 +1,10 @@
+import Image from "next/image"
+import PFP from "../public/icon.png"
+
 export default function HomePage() {
     return (
         <div className="homeStyling">
+            <Image src={PFP} alt="pfp" width="100" height="100"/>
             <h1>Hello There,</h1>
 
             <h2>welcome to my isolated node in the internet. Perhaps it will become more connected one day, or not.</h2>
