d69668bcdec8622dc118d8de5b12cf23dd82d4cb
mateedo <mateoshkodra9@gmail.com>
2025-07-26 01:52:35 -0700

    chore: build issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
       <div>
         <p>Hi, my name is Mateo Shkodra</p>
-        <p>I'm a student living in San Francisco</p>
+        <p>I&apos;m a student living in San Francisco</p>
         <br/>
         <p>Email: <a href="mailto:me@mshkodra.com" className="text-blue-600 hover:underline">me@mshkodra.com</a></p>
         
