d4aa40f706aecf377415dba7569765f9b523ecb0
mateedo <mateoshkodra9@gmail.com>
2025-08-12 01:25:58 -0700

    feat: ver 2

--- public/better-radius.js
+++ public/better-radius.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BetterRadius
 // @namespace    https://mshkodra.com/
-// @version      0.1.5
+// @version      0.2
 // @description  Better version of Radius.  
 // @author       mshkodra
 // @match        https://radius.mathnasium.com/*
